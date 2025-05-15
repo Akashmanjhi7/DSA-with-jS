eae00bf541c31ed697928cd16da02fa472949f86
akash <akashmanjhi1111198@gmail.com>
2025-05-16 00:08:47 +0530

    push K element question

--- Vid_6 Advance Array/scipt.js
+++ Vid_6 Advance Array/scipt.js
@@ -21,22 +21,58 @@
 
 // left Rotation by K Element
 
-function leftRoataionByK (k,arr){
-for(let i=0 ; i<k; i++){
-    let arrCopy = arr[0]
-    for(let j=0 ; j<arr.length-1;j++ ){
-            arr[j] = arr[j+1]
-    }
-    arr[arr.length-1] = arrCopy
+// function leftRoataionByK (ks,arr){
+//     let k = ks%arr.length
+//     let count = 0
+// for(let i=0 ; i<k; i++){
+//     count++
+//     let arrCopy = arr[0]
+//     for(let j=0 ; j<arr.length-1;j++ ){
+//             arr[j] = arr[j+1]
+//     }
+//     arr[arr.length-1] = arrCopy
 
+// }
+// console.log(count)
+// return arr
+// }
+// const array = [1,2,3,4,5]
+// console.log(leftRoataionByK(8,array))
+
+
+
+
+// Above Solution is brute force solution 
+
+// New Solution with Maths Algorithum
+
+
+// const rotateArray = (k,arr) =>{
+//     // this is left rotation
+// let tempArr = Array(arr.length)
+
+// for(let i=0; i<arr.length; i++){
+//     tempArr[i]= arr[(i+k)%arr.length]
+// }
+// console.log(tempArr)
+// }
+
+
+// let array = [1,2,3,4,5]
+// rotateArray(2,array)
+
+
+const rotateArrayRight = (k,arr)=>{
+    let tempArr = Array(arr.length)
+for(let i=0; i<arr.length;i++){
+tempArr[(i+k)%arr.length] = arr[i]
 }
-return arr
+
+console.log(tempArr)
 }
-const array = [1,2,3,4,5]
-console.log(leftRoataionByK(2,array))
 
-
-
+let array = [1,2,3,4,5]
+rotateArrayRight(2,array)
 
 
 
